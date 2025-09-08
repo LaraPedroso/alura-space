7b826973bf147065e9d2aeadb36fed1eeb3fd792
Lara <pedroso5@hotmail.com>
2025-09-08 16:56:10 -0300

    Adiciona modelo de imagem e atualiza a visualização da galeria com dados dinâmicos

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1:{'nome':'Imagem 1', 'descricao':'Descrição da Imagem 1', 'caminho':'galeria/imagem1.jpg'},
+        2:{'nome':'Imagem 2', 'descricao':'Descrição da Imagem 2', 'caminho':'galeria/imagem2.jpg'},
+    }
+
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 
 def imagem(request):
