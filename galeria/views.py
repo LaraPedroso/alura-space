8e13708f842d0ffff1e15326c50c29483dec5e94
Lara <pedroso5@hotmail.com>
2025-09-10 08:50:49 -0300

    Atualiza a lógica de visualização da galeria, ajusta as URLs e melhora a renderização das imagens dinâmicas

--- galeria/views.py
+++ galeria/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Imagem
 
 
 def index(request):
-    dados = {
-        1:{'nome':'Imagem 1', 'descricao':'Descrição da Imagem 1', 'caminho':'galeria/imagem1.jpg'},
-        2:{'nome':'Imagem 2', 'descricao':'Descrição da Imagem 2', 'caminho':'galeria/imagem2.jpg'},
-    }
-
-    return render(request, 'galeria/index.html', {'cards': dados})
+    fotografias = Imagem.objects.all()
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Imagem, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
