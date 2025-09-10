fb6e441d20ed816d88efc8ed3fb10eb3b6f0d84d
Lara <pedroso5@hotmail.com>
2025-09-10 09:25:41 -0300

    Adiciona campos 'categoria' e 'legenda' ao modelo de imagem, atualiza a visualização da imagem para exibir a legenda

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from galeria.models import Imagem
 
-# Register your models here.
+class listaImagens(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda','descricao', 'categoria', 'foto')
+    list_display_links = ('id', 'nome', 'descricao' )
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_per_page = 10
+
+
+
+admin.site.register(Imagem, listaImagens)
--- galeria/models.py
+++ galeria/models.py
@@ -3,7 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Imagem(models.Model):
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=100, null=True, blank=True)
+    categoria = models.CharField(max_length=50, choices=OPCOES_CATEGORIA, default="")
     descricao = models.TextField(max_length=150, null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
